906d44a5d5753ca2f3ce8e24dfc5cd86a9c09b46
thiranat <thiranat-srs@Thiranat-SRS.local>
2025-08-27 13:54:07 +0700

    [fix] Access-Control-Allow-Origin

--- golang-backend/main.go
+++ golang-backend/main.go
@@ -8,6 +8,12 @@ import (
 
 func main() {
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:5173")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Next()
+	})
 	r.POST("/upload", controller.UploadHandler)
 	r.Run(":8080")
 }
